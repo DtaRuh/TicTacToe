d03df54ec188af5f0186cdfa0f0a39ddea1293f1
Dtaruh <DaraMoylan@gmail.com>
2025-10-02 15:03:36 +0100

    Added a method that returns true if a player has won the game

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,14 +1,19 @@
 import java.util.Scanner;
+import java.util.Random;
 
 
 public class TicTacToe {
 
+
+    // Initialising Scanner and Random objects for further methods
     public static final Scanner keyboard = new Scanner(System.in);
+    public static final Random randomNum = new Random();
      
     //Set pointers for each player plus the empty positions
     private static final char PLAYER_X = 'X';
     private static final char PLAYER_O = 'O';
     private static final char EMPTY = ' ';
+
 
     // initialise the our tic tac toe board
     public static char[][] board = new char[3][3];
@@ -75,12 +80,95 @@ public class TicTacToe {
         System.out.print("Player " + currentPlayer + ", choose position (1-9): ");
         return keyboard.nextInt();
     }
+
+    // I want to check if the space is free or occupied by a player
+    // and if so return a true or false
+    /* 
+     *@param intakes the users choice of index for a marker
+      @return returns a boolean true or false
+     */
+    public static boolean checkChoice(int choice) { 
+        if(choice >= 1 && choice <= 9) { 
+            int row = (choice - 1) / 3;
+            int col = (choice - 1) % 3;
+
+            if(board[row][col] == EMPTY) { 
+                return true;
+            }
+            else { 
+                return false;
+            }
+        }
+        else { 
+            return false;
+        }
+    }
+
+    /*
+     * method to choose which player to begin the game
+     * 1 == Player_X
+     * 2 == Player_O
+     * @param function takes no argument
+     * @return return an integer 1 or 2
+     */
+    public static int choosePlayer() { 
+        int n = randomNum.nextInt(2)+1;
+        return n;
+    }
+
+    /* 
+     * Method to check if the game has been won
+     * @param takes the 2D array as the argument
+     * @return return true if game has been won and increment checkWin variable, return false if not
+     */
+    public static boolean checkWinner(char[][] board) { 
+        // Horizontal logic
+        for(int i = 0; i < board.length; i++)  {
+            if(board[i][0] == board[i][1] && board[i][0] == board[i][2] && board[i][0] != EMPTY)  {
+                return true;
+            }
+        }
+        // vertical logic
+        // need to be able to loop through the array to check indexes []
+        for(int j = 0; j < board.length; j++) { 
+            if(board[0][j] == board[1][j] && board[2][j] == board[0][j] && board[0][j] != EMPTY) { 
+                return true;
+            }
+        }
+
+        // Diagonal logic only 2 diagonals so lets check it out 
+        if(board[0][0] == board[1][1] && board[0][0] == board[2][2] && board[0][0] != EMPTY) { 
+            return true;
+        }
+        else if (board[0][2] == board[1][1] && board[0][2] == board[2][0] && board[0][2] != EMPTY) { 
+            return true;
+        }
+        return false;
+    }
+
+
     
      
     public static void main(String[] args)  {
 
         resetBoard(board);
-        printBoard(board); 
+        printDisplayBoard();
+        
+        if(choosePlayer() == 1) { 
+            getPlayerMove(PLAYER_X);
+        } 
+        else { 
+            getPlayerMove(PLAYER_O);
+        }
+
+        do  {
+            checkChoice(getPlayerMove(PLAYER_X)); 
+            checkWinner();
+            printBoard(board);
+
+        }
+        while (checkWinner(board) != true);
+
     }
 }
 
